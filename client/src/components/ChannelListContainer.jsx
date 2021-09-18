95dddb8ba83b51a601acea9163cad44117ac8333
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-18 21:52:42 +0530

    commit

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import { ChannelList, userChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
+
+const sidebar = ()=>
+{
+    <div className="channel-list__sidebar">
+        
+    </div>
+}
+
 const ChannelListContainer = () => {
     return (
         <div>
