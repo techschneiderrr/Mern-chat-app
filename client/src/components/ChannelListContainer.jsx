ed28eadbbfdc23c58f46b4b9725f6fc17dd8f835
Manav Deep Singh Lamba <manavdeep.singhlamba@gmail.com>
2021-09-21 01:05:26 +0530

    commit

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,22 +3,61 @@ import { ChannelList, userChatContext } from 'stream-chat-react';
 import Cookies from 'universal-cookie';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 
-const sidebar = ()=>
-{
+import HospitalIcon from '../assets/hospital.png'
+import LogoutIcon from '../assets/logout.png'
+
+const SideBar = ()=>
+(
     <div className="channel-list__sidebar">
-        <div className="channel-lis__sidebar__icon1">
-            <div className="icon1__innr">
-                <img src={HospitalIcon} alt="hospiatl" width="30" />
+        <div className="channel-list__sidebar__icon1">
+            <div className="icon1__inner">
+                <img src={HospitalIcon} alt="hospital" width="30" />
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img src={LogoutIcon} alt="logout" width="30" />
             </div>
         </div>
     </div>
-}
+);
+
+const CompanyHeader = () =>
+(
+    <div className="channel-list__header">
+        <p className="channel-list__header__text" style={{textAlign:'center'}}>
+            Medical Pager
+        </p>
+    </div>
+);
 
 const ChannelListContainer = () => {
     return (
-        <div>
-            ChannelListContainer
-        </div>
+        <>
+            <SideBar />
+            <div className="channel-list__list__wrapper">
+                <CompanyHeader />
+                <ChannelSearch />
+                <ChannelList 
+                    filters={{}}
+                    channelRenderFilterFn={()=>{}}
+                    List=
+                    {(listProps)=>
+                        (
+                            <TeamChannelList 
+                                {...listProps}
+                                type="team"
+                            />
+                        )}
+                        Preview={(previewprops)=>(
+                            <TeamChannelPreview 
+                                {...previewprops}
+                                type="team"
+                            />
+                        )}
+                />
+            </div>
+        </>
     )
 }
 
